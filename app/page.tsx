8f20cb89a0e94a155c4db2176de2983f4cde220a
Torex9 <92604396+Torex9@users.noreply.github.com>
2024-06-02 22:48:52 +0100

    added cards to projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -8,11 +10,10 @@ export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
