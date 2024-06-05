b247f31c6212361dce26cda1b15afd2e34112c24
Torex9 <92604396+Torex9@users.noreply.github.com>
2024-06-05 23:46:44 +0100

    started work on favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,33 @@ import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
+//fix the correct icons later
+
 export const metadata: Metadata = {
-  title: "Rufus Portfolio",
+  title: {
+    default: "Rufus' Portfolio",
+    template: "%s | Rufus' Portfolio",
+  },
   description: "Rufus' portfolio",
+  icons: {
+    icon: "/favicons/favicon-32x32.png",
+    apple: "/apple-icon.png",
+    other: [
+      {
+        rel: "apple-touch-icon",
+        url: "/favicons/apple-touch-icon.png",
+      },
+      {
+        rel: "mask-icon",
+        url: "/favicons/safari-pinned-tab.svg",
+        color: "#ed5c82",
+      },
+      {
+        rel: "shortcut icon",
+        url: "/favicons/favicon.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
